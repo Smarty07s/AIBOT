dcfe9e0ecf144caf16f5347d6dbb77ec1374b6a4
Smarty07s <soumyamishra2099@outlook.com>
2022-12-07 01:20:56 +0530

    OK

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import nltk
 from nltk.tokenize import sent_tokenize,word_tokenize
+from  nltk.corpus import stopwords
 
-
-query = str(input("Query: "))
+query = (str(input("Query: "))).lower()
 
 sent_tok = sent_tokenize(query)
 word_tok = word_tokenize(query)
+print(stopwords.words('english'))
 
 
 print(sent_tok,word_tok)
