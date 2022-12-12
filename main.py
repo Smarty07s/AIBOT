3ac1aab47e4073d9f6a16773e1c1cc29e9a41dc4
Smarty07s <soumyamishra2099@outlook.com>
2022-12-12 23:17:27 +0530

    OK

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ from  nltk.corpus import stopwords
 
 query = (str(input("Query: "))).lower()
 
-sent_tok = sent_tokenize(query)
+sent_seg = sent_tokenize(query)
 word_tok = word_tokenize(query)
 # print(stopwords.words('english'))
 
-print(os.system('notepad'))
- 
+b=2
+for i in range(b):
+    if os.path.exists(f'corp/raw/doc{i}.txt')==True:
+        print(True)
+        b+=1
+    elif os.path.exists(f'corp/raw/doc{i}.txt')==False:
+        print(False)
+        document = open(f'corp/raw/doc{i}.txt','w')
+        document.write(query)
+        document.close()
+        document = open(f'corp/sent_seg/doc{i}.txt','w')
+        document.write(str(sent_seg))
+        document.close()
+        document = open(f'corp/word_tok/doc{i}.txt','w')
+        document.write(str(word_tok))
+        break
