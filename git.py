4fe35602078f60f1b6e15562ecabf8953380d131
Smarty07s <soumyamishra2099@outlook.com>
2022-12-12 21:35:37 +0530

    OK

--- git.py
+++ git.py
@@ -2,7 +2,7 @@ import os
 
 
 
-
+os.system('git set-url origin https://github.com/Smarty07s/AIBOT.git')
 os.system('git add *')
 commit = input('Commit desc: ')
 os.system(f'git commit -m "{commit}"')
