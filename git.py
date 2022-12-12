5e055e1b373fcbd9ed27c194836f6b86ef94cc1e
Smarty07s <soumyamishra2099@outlook.com>
2022-12-12 21:32:12 +0530

    OK

--- git.py
+++ git.py
@@ -1,6 +1,9 @@
 import os
 
-commit = input('Commit desc: ')
+
+
 os.system('git add *')
+os.system('git add *')
+commit = input('Commit desc: ')
 os.system(f'git commit -m "{commit}"')
 os.system('git push *')
