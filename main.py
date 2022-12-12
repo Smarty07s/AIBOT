066527544430820adc4d491d8bab5f00c4449ead
Smarty07s <soumyamishra2099@outlook.com>
2022-12-12 23:48:21 +0530

    OK

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import nltk
 import os
-from nltk.tokenize import sent_tokenize,word_tokenize
-from  nltk.corpus import stopwords
+import json
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
 query = (str(input("Query: "))).lower()
 
@@ -9,19 +10,22 @@ sent_seg = sent_tokenize(query)
 word_tok = word_tokenize(query)
 # print(stopwords.words('english'))
 
-b=2
+b = 2
 for i in range(b):
-    if os.path.exists(f'corp/raw/doc{i}.txt')==True:
+    if os.path.exists(f'corp/raw/doc{i}.txt') == True:
         print(True)
-        b+=1
-    elif os.path.exists(f'corp/raw/doc{i}.txt')==False:
+        b += 1
+    elif os.path.exists(f'corp/raw/doc{i}.txt') == False:
         print(False)
-        document = open(f'corp/raw/doc{i}.txt','w')
+        document = open(f'corp/raw/doc{i}.txt', 'w')
         document.write(query)
         document.close()
-        document = open(f'corp/sent_seg/doc{i}.txt','w')
+        document = open(f'corp/sent_seg/doc{i}.txt', 'w')
         document.write(str(sent_seg))
         document.close()
-        document = open(f'corp/word_tok/doc{i}.txt','w')
+        document = open(f'corp/word_tok/doc{i}.txt', 'w')
         document.write(str(word_tok))
         break
+
+
+
