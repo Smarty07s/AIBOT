6c0da09e6b16c9ec1e31c5a9cd57e244375f32c6
Smarty07s <soumyamishra2099@outlook.com>
2022-12-13 21:50:14 +0530

    OK

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,4 +4,17 @@ def createf(filename , data):
         f.write(data)
         f.close()
 
-        
+class abcd():
+    def __init__(self) -> None:
+        return None
+    
+    def tfidf(self,word,documents):
+        for i in documents:
+            d = open(i,'r')
+            r = list(d.read())
+            for a in r:
+                if word in r:
+                    print(word)
+                print(a)
+
+print(abcd().tfidf('and',['doc0.txt']))
